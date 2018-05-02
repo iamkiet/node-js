beaafc0aa424107cf7098a0720e7332bea15384e
kietnguyen <kietnguyen.me@gmail.com>
2018-05-02 16:57:09 +0700

    enable access control allow

--- celebrities-back-to-front/models/connectionDB.js
+++ celebrities-back-to-front/models/connectionDB.js
@@ -5,6 +5,7 @@ var pool      =    mysql.createPool({
     connectionLimit : 10,
     host     : '127.0.0.1',
     user     : 'root',
+    password : ' ',
     database : 'showbiz',
     debug    :  false
 });
